ad484e1dd302d8f0a6e860a862e9cbc112dd81eb
SIMON WACHIRA <simonwachira7318@gmail.com>
2023-07-27 14:37:41 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// function talk() {
+//     var know = {
+//         "hello": "Hello too, what do you have for today...",
+//         "who are you": " Hello, Am an AI created by Simon",
+//         "how are you": "Am Good, Looking forward to serve you,,",
+//         "ok": "Great to serve you",
+//         "bye": "Okay!, Always,Ready to serve you..😂 ",
+//         "what can you do": "i can answer you questions😂😂",
+
+//    };
+//    var user = 
+//     document.getElementById("userbox").value 
+//     .tolowerCase();
+//     document.getElementById("chatLog").innerHTML =
+//      user + "<br>";
+//      if (user in know) {
+//         document.getElementById("chatLog").innerHTML =
+//         know[user] + "<bbr>";
+//      }else {
+//         document.getElementById(chatLog).innerHTML =
+//         "sorry, Am still learning <br>";
+//      }
+
+
+// }
